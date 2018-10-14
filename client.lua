0fe1936ebd95a703cbbe54499cbbc4a6a8b0d44a
Jonas Svensson <jonassvensson4@hotmail.com>
2018-10-14 13:07:26 +0200

    Clean up

--- client.lua
+++ client.lua
@@ -1,5 +1,4 @@
 local ESX      = nil
-local open 		 = false
 local inMarker = false
 local PlayerData = {}
 
@@ -30,19 +29,19 @@ end
 
 -- Display marker and Enter/Exit events
 Citizen.CreateThread(function ()
-  while true do
-    Wait(0)
+	while true do
+    		Wait(0)
 		if PlayerData.job ~= nil and PlayerData.job.name ~= 'unemployed' and PlayerData.job.name == "police" then
 			v = Config.Marker
-	    if( v.Type ~= -1 and GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), v.Pos.x, v.Pos.y, v.Pos.z, true) < 50 ) then
-	      DrawMarker(v.Type, v.Pos.x, v.Pos.y, v.Pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.Size.x, v.Size.y, v.Size.z, v.Color.r, v.Color.g, v.Color.b, 100, false, true, 2, false, false, false, false)
-	    end
+			if( v.Type ~= -1 and GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), v.Pos.x, v.Pos.y, v.Pos.z, true) < 50 ) then
+				DrawMarker(v.Type, v.Pos.x, v.Pos.y, v.Pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.Size.x, v.Size.y, v.Size.z, v.Color.r, v.Color.g, v.Color.b, 100, false, true, 2, false, false, false, false)
+			end
 			if( v.Type ~= -1 and GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), v.Pos.x, v.Pos.y, v.Pos.z, true) < v.Size.x ) then
-	      inMarker = true
+				inMarker = true
 				hintToDisplay(v.Hint)
 			else
 				inMarker = false
-	    end
+			end
 		end
 	end
 end)
@@ -54,7 +53,6 @@ Citizen.CreateThread(function()
 		if IsControlJustReleased(0, 38) and inMarker then
 			ESX.TriggerServerCallback('jsfour-criminalrecord:fetch', function( d )
 				SetNuiFocus(true, true)
-				open = true
 
 				SendNUIMessage({
 				  action = "open",
@@ -65,26 +63,9 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- Disable movements when the NUI is open
-Citizen.CreateThread(function()
-	SetNuiFocus(false, false)
-  while true do
-    if open then
-      DisableControlAction(0, 1, true) -- LookLeftRight
-      DisableControlAction(0, 2, true) -- LookUpDown
-      DisableControlAction(0, 24, true) -- Attack
-      DisablePlayerFiring(GetPlayerPed(-1), true) -- Disable weapon firing
-      DisableControlAction(0, 142, true) -- MeleeAttackAlternate
-      DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
-    end
-    Citizen.Wait(0)
-  end
-end)
-
 -- NUI Callback - Close
 RegisterNUICallback('escape', function(data, cb)
 	SetNuiFocus(false, false)
-	open = false
 	cb('ok')
 end)
 
